b4716baac3c185ed02d5519e3121592d34da0236
tavisca-vchauhan <vchauhan@tavisca.com>
2018-09-13 18:59:10 +0530

    Tic Tac Toe API implementation with AOP and Cross cutting concept

---
+++ TicTacToe/TicTacToe/Controllers/IdentityController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace TicTacToe.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Identity")]
+    public class IdentityController : Controller
+    {
+        [HttpPost]
+        [Log]
+        public IActionResult RegisterUser([FromBody] User user)
+        {
+            Database database = new Database();
+            User userResponse = new User();
+            Guid guid = Guid.NewGuid();
+            string _token = Convert.ToBase64String(guid.ToByteArray());
+            _token = _token.Replace("=", "");
+            _token = _token.Replace("+", "");
+            userResponse = database.PostUserRequest(user.FirstName, user.LastName, user.Username, _token);
+            if (userResponse.RegisterMessage.Equals("Failure"))
+            {
+                LogAttribute.Request = "Add User";
+                LogAttribute.Response = "Failure";
+                LogAttribute.Exception = "Bad Request";
+                LogAttribute.Comment = "User Failed to add into database";
+                return BadRequest(userResponse);
+            }
+            LogAttribute.Request = "Add User";
+            LogAttribute.Response = "Success";
+            LogAttribute.Exception = "No Exception";
+            LogAttribute.Comment = "User Successfully added into database";
+            return Ok(userResponse);
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/Logger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Logger
+    {
+        private static Logger _Logger = null;
+        private static Object _classLock = typeof(Logger);
+        Database database = new Database();
+        private Logger()
+        {
+
+        }
+
+        public static Logger getInstance()
+        {
+            //lock object to make it thread safe  
+            lock (_classLock)
+            {
+                if (_Logger == null)
+                {
+                    _Logger = new Logger();
+
+                }
+            }
+            return _Logger;
+        }
+
+        public void LoggingDetails(string request, string response, string exception, string comment)
+        {
+            database.AddLoggingDetails(request, response, exception, comment);
+        }
+    }
+}
